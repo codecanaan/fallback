33c240ac6895ffd32d08d01d627fd9150df4570f
lyhcode <lyhcode@gmail.com>
2013-01-17 14:52:34 +0800

    更新 php 程式

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-fallback
-========
+Fallback Services for CodeCanaan
+================================
 
-Fallback Services
+此程式用於協助 CodeCanaan 處理網站錯誤無法復原的問題。
+
+1. 清除瀏覽器 Cookies 資料
+2. 重新導向來源頁面或首頁
+
--- index.php
+++ index.php
@@ -11,15 +11,22 @@ if (isset($_SERVER['HTTP_COOKIE'])) {
 }
 
 //header('Location: '.$_SERVER['HTTP_REFERER']);
+
+if (isset($_SERVER['HTTP_REFERER'])) {
+	$redirect_url = $_SERVER['HTTP_REFERER'];
+}
+else {
+	$redirect_url = 'http://codecanaan.com/';
+}
 ?>
 <html>
 <head>
-<meta http-equiv="refresh" content="0;url=<?php echo $_SERVER['HTTP_REFERER'] ?>">
+<meta http-equiv="refresh" content="0;url=<?php echo $redirect_url ?>">
 <script type="text/javascript">
-location.href='<?php echo $_SERVER['HTTP_REFERER'] ?>';
+location.href='<?php echo $redirect_url; ?>';
 </script>
 </head>
 <body>
-Redirect to <a href="<?php echo $_SERVER['HTTP_REFERER'] ?>"><?php echo $_SERVER['HTTP_REFERER'] ?></a>
+Redirect to <a href="<?php echo $redirect_url; ?>"><?php echo $redirect_url; ?></a>
 </body>
 </html>
